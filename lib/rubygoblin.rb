e3d488086087fc000cd9e3d344e217d542e32ca0
Cody Johnston <cody@codegobl.in>
2012-02-29 15:44:05 -0800

    formatting

--- lib/rubygoblin.rb
+++ lib/rubygoblin.rb
@@ -2,10 +2,7 @@ module Rubygoblin
 
   RUBYGOBLIN_PATH = File.dirname(__FILE__) + "/rubygoblin/"
 
-  [
-    "object",
-    "kernel"
-  ].each do |lib|
+  [ "object", "kernel" ].each do |lib|
     require RUBYGOBLIN_PATH + lib
   end
 
